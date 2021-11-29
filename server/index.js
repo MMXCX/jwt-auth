79ac2453693f82f3e2d1cd27ed8bcc32bfaf0de4
MMXCX <mmxcx@gmail.com>
2021-11-30 02:36:40 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
+const mongoose = require("mongoose")
 
 const PORT = process.env.PORT || 5000
 const app = express()
